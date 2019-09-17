b0d61ce4567024877290fcfb066fa248ac9314b8
Vincent Manoli <vinomano@ut.utm.edu>
2019-09-16 20:32:43 -0700

    a brief message

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,37 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rand = new Random();
+            int Rand = rand.Next(0, 100);
+
+            int input;
+            int guesses = 0;
+
+            Console.WriteLine("Guess my number!");
+            input =  Convert.ToInt32(Console.ReadLine());
+
+            while (input != Rand) {
+
+                if (input < Rand)
+                {
+                    Console.WriteLine("Guess too low");
+                    guesses++;
+                    Console.WriteLine("Guess again");
+                    input = Convert.ToInt32(Console.ReadLine());
+                }
+                else if (input > Rand)
+                {
+                    Console.WriteLine("Guess too high");
+                    guesses++;
+                    Console.WriteLine("Guess again");
+                    input = Convert.ToInt32(Console.ReadLine());
+                }
+                
+
+            }
+            guesses++;
+            Console.WriteLine($"Thats right you got it in {guesses} tries!!");
+            Console.ReadKey();
         }
     }
 }
